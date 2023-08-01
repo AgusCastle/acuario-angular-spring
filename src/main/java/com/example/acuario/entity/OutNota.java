7218289f9eedb66836d0689f10f2184b3de6e5b7
Agus <agu13xd@hotmail.com>
2023-08-01 15:19:44 -0600

    'PDF'

---
+++ src/main/java/com/example/acuario/entity/OutNota.java
@@ -0,0 +1,61 @@
+package com.example.acuario.entity;
+
+public class OutNota{
+    private int cantidad;
+    private String description;
+    private String precio_uni;
+    private String precio_total;
+    private String total;
+
+    public OutNota() {
+    }
+
+    public String getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public OutNota(int cantidad, String description, String precio_uni, String precio_total, String total) {
+        this.cantidad = cantidad;
+        this.description = description;
+        this.precio_uni = precio_uni;
+        this.precio_total = precio_total;
+        this.total = total;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrecio_uni() {
+        return this.precio_uni;
+    }
+
+    public void setPrecio_uni(String precio_uni) {
+        this.precio_uni = precio_uni;
+    }
+
+    public String getPrecio_total() {
+        return this.precio_total;
+    }
+
+    public void setPrecio_total(String precio_total) {
+        this.precio_total = precio_total;
+    }
+}
--- src/main/java/com/example/acuario/util/ReportGenerator.java
+++ src/main/java/com/example/acuario/util/ReportGenerator.java
@@ -1,31 +1,57 @@
 package com.example.acuario.util;
 
+import com.example.acuario.entity.OutNota;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.engine.JasperCompileManager;
 
 @Service
 public class ReportGenerator {
-    private JasperPrint getReport() throws FileNotFoundException, JRException{
+    private JasperPrint getReport(List<OutNota> datos) throws FileNotFoundException, JRException{
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("nombre_cliente", "Alfonso Morado");
         params.put("fecha_string", "12/07/2023");
         params.put("folio", "43251");
 
-        InputStream jasperStream = getClass().getClassLoader().getResourceAsStream("jasper/Nota.jasper");
+        InputStream jasperStream = getClass().getClassLoader().getResourceAsStream("jasper/Nota.jrxml");
+        JasperDesign jasperDesign = JRXmlLoader.load(jasperStream);
+        JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
 
-        return JasperFillManager.fillReport(jasperStream, params);
+        JRBeanCollectionDataSource items = new JRBeanCollectionDataSource(datos);
+        params.put("CollectionBeanDataParam", items);
+
+        return JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
     }
 
     public byte[] exportPDF() throws JRException, FileNotFoundException{
-        return JasperExportManager.exportReportToPdf(getReport());
+        List<OutNota> datos = new ArrayList<OutNota>();
+        OutNota d1 = new OutNota(2, "Filtro", "$12,345.50", "12,345.50", "49,382.00");
+        OutNota d2 = new OutNota(3, "Mantenimiento marino", "$12,345.50", "12,345.50", "49,382.00");
+        OutNota d3 = new OutNota(1, "Agua bidestilla", "$12,345.50", "12,345.50", "49,382.00");
+        OutNota d4 = new OutNota(1, "Filtro corrugado", "$12,345.50", "12,345.50", "49,382.00");
+        
+        datos.add(d1);
+        datos.add(d2);
+        datos.add(d3);
+        datos.add(d4);
+
+        return JasperExportManager.exportReportToPdf(getReport(datos));
     }
 }
