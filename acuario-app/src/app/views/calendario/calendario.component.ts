b236085756acb2a9de9681f0cc3f021e22e78259
Agus Castillo <agu13xd@hotmail.com>
2023-08-04 17:53:40 -0600

    Calendar

--- acuario-app/src/app/views/calendario/calendario.component.ts
+++ acuario-app/src/app/views/calendario/calendario.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
-
+import interactionPlugin from '@fullcalendar/interaction';
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
@@ -10,7 +10,19 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 export class CalendarioComponent {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
-    locale: 'es'
+    plugins: [dayGridPlugin, interactionPlugin],
+    locale: 'es',
+    events: [
+      { title: 'event 1', date: '2023-08-05' },
+      { title: 'event 2', date: '2023-08-05' }
+    ],
+    dateClick: function(info: any) {
+      alert('Has hecho clic en el día ' + info.date);
+    },
+    eventClick: this.handleDateClick.bind(this),
   };
+
+  handleDateClick(arg: any) {
+    alert('date click! ' + arg.dateStr)
+  }
 }
